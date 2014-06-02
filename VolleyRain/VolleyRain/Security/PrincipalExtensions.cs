983cb0d995d50fc35883e127743465655d198238
Raphael Fleischlin <git@raeffs.dev>
2014-06-02 22:09:17 +0200

    admin menu

---
+++ VolleyRain/VolleyRain/Security/PrincipalExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace VolleyRain.Extensions
+{
+    public static class PrincipalExtensions
+    {
+        public static bool IsAdministrator(this IPrincipal principal)
+        {
+            return principal.IsInRole("Administrator");
+        }
+    }
+}
