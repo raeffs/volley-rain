fad88d95693e64a1ff496abdc8792c2ca0293c4e
Raphael Fleischlin <git@raeffs.dev>
2014-07-23 15:30:47 +0200

    calendar navigation

--- VolleyRain/VolleyRain/Models/Month.cs
+++ VolleyRain/VolleyRain/Models/Month.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,16 @@ namespace VolleyRain.Models
 {
     public class Month
     {
+        [DisplayFormat(DataFormatString = "{0:MMMM yyyy}")]
         public DateTime Date { get; set; }
+
         public IEnumerable<Day> Days { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MMMM yyyy}")]
+        public DateTime NextMonth { get { return Date.AddMonths(1); } }
+
+        [DisplayFormat(DataFormatString = "{0:MMMM yyyy}")]
+        public DateTime PreviousMonth { get { return Date.AddMonths(-1); } }
 
         public Month()
         {
