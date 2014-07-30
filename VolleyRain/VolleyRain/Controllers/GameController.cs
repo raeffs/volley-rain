15a746dba704d115ec0b96432557b5d44d58843d
Raphael Fleischlin <git@raeffs.dev>
2014-07-30 17:37:30 +0200

    game

--- VolleyRain/VolleyRain/Controllers/GameController.cs
+++ VolleyRain/VolleyRain/Controllers/GameController.cs
@@ -12,7 +12,10 @@ namespace VolleyRain.Controllers
         public ActionResult Index(int? teamID)
         {
             var season = Cache.GetSeason(() => Context.Seasons.GetActualSeason());
-            var games = Context.Events.Where(e => e.Team.Season.ID == season.ID && e.Details != null).ToList();
+            var games = Context.Events
+                .Where(e => e.Team.Season.ID == season.ID && e.Details != null)
+                .Select(e => new Game { Start = e.Start, HomeTeam = e.Details.HomeTeam, GuestTeam = e.Details.GuestTeam, Hall = e.Details.Hall })
+                .ToList();
 
             return View(games);
         }
---
+++ VolleyRain/VolleyRain/Models/ViewModels/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VolleyRain.Models
+{
+    public class Game
+    {
+        public DateTime Start { private get; set; }
+
+        [Display(Name = "Datum")]
+        [DisplayFormat(DataFormatString = "{0:ddd, dd. MMMM}")]
+        public DateTime StartDate { get { return Start; } }
+
+        [Display(Name = "Zeit")]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}")]
+        public DateTime StartTime { get { return Start; } }
+
+        [Display(Name = "Heim")]
+        public string HomeTeam { get; set; }
+
+        [Display(Name = "Gast")]
+        public string GuestTeam { get; set; }
+
+        [Display(Name = "Halle")]
+        public string Hall { get; set; }
+    }
+}
