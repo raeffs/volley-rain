c605659beab5de0b29cec95dc2cfaac712dde749
Raphael Fleischlin <git@raeffs.dev>
2014-07-30 20:27:49 +0200

    pagination

--- VolleyRain/VolleyRain/Controllers/AttendanceController.cs
+++ VolleyRain/VolleyRain/Controllers/AttendanceController.cs
@@ -10,14 +10,29 @@ namespace VolleyRain.Controllers
 {
     public class AttendanceController : BaseController
     {
+        public AttendanceController()
+        {
+            ViewBag.PageSize = 10;
+        }
+
         [Authorize(Roles = "User")]
         public ActionResult Index(int? teamID, int? page)
         {
             var season = Cache.GetSeason(() => Context.Seasons.GetActualSeason());
             var teamIDs = Context.Teams.Where(t => t.Season.ID == season.ID).Select(t => t.ID).ToList();
+
+            var pagination = new Pagination(
+                10,
+                Context.Events.Count(e => teamIDs.Contains(e.Team.ID)),
+                page);
+            if (!page.HasValue) pagination.JumpToItem(Context.Events.Count(e => teamIDs.Contains(e.Team.ID) && e.Start < DateTime.Today) + 1);
+            ViewBag.Pagination = pagination;
+
             var events = Context.Events
-                .Where(e => e.Start >= DateTime.Today && teamIDs.Contains(e.Team.ID))
-                .Take(10)
+                .Where(e => teamIDs.Contains(e.Team.ID))
+                .OrderBy(e => e.Start)
+                .Skip(pagination.ItemsToSkip)
+                .Take(pagination.PageSize)
                 .ToList();
             var users = Context.Teams
                 .Where(t => teamIDs.Contains(t.ID))
---
+++ VolleyRain/VolleyRain/Models/ViewModels/Pagination.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VolleyRain.Models
+{
+    public class Pagination
+    {
+        public Pagination(int pageSize, int totalItemCount, int? currentPage)
+        {
+            PageSize = pageSize;
+            TotalItemCount = totalItemCount;
+            PageCount = (int)Math.Ceiling(TotalItemCount / (double)PageSize);
+            CurrentPage = currentPage.HasValue && currentPage.Value >= 1 && currentPage.Value <= PageCount ? currentPage.Value : 1;
+        }
+
+        public int PageSize { get; private set; }
+
+        public int TotalItemCount { get; private set; }
+
+        public int PageCount { get; private set; }
+
+        public int CurrentPage { get; private set; }
+
+        public bool HasPreviousPage { get { return CurrentPage > 1; } }
+
+        public int PreviousPage { get { return CurrentPage - 1; } }
+
+        public bool HasNextPage { get { return CurrentPage < PageCount; } }
+
+        public int NextPage { get { return CurrentPage + 1; } }
+
+        public int ItemsToSkip { get { return (CurrentPage - 1) * PageSize; } }
+
+        public void JumpToItem(int index)
+        {
+            CurrentPage = (int)Math.Ceiling(index / (double)PageSize);
+        }
+    }
+}
