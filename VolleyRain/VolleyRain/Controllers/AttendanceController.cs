059b68ac7edb66c6b52551cd5e9cc1e1d47a2ba4
Raphael Fleischlin <git@raeffs.dev>
2014-07-31 13:13:25 +0200

    attendance

--- VolleyRain/VolleyRain/Controllers/AttendanceController.cs
+++ VolleyRain/VolleyRain/Controllers/AttendanceController.cs
@@ -51,7 +51,10 @@ namespace VolleyRain.Controllers
         [Authorize(Roles = "User")]
         public ActionResult Edit()
         {
-            ViewBag.AttendanceTypes = new SelectList(Cache.GetAttendanceTypes(() => Context.AttendanceTypes.ToList()), "ID", "Name");
+            var noSelection = new AttendanceType { ID = 0, Name = string.Empty };
+            var attendanceTypes = Cache.GetAttendanceTypes(() => Context.AttendanceTypes.ToList()).ToList();
+            attendanceTypes.Add(noSelection);
+            ViewBag.AttendanceTypes = attendanceTypes.OrderBy(t => t.ID).ToList();
 
             var season = Cache.GetSeason(() => Context.Seasons.GetActualSeason());
             var teamIDs = Context.Teams.Where(t => t.Season.ID == season.ID).Select(t => t.ID).ToList();
@@ -70,10 +73,67 @@ namespace VolleyRain.Controllers
 
             foreach (var attendance in Context.Attendances.Where(a => a.User.ID == Session.UserID && ids.Contains(a.Event.ID)))
             {
-                model.Single(m => m.ID == attendance.Event.ID).AttendanceType = attendance.Type;
+                var item = model.Single(m => m.ID == attendance.Event.ID);
+                item.AttendanceType = attendance.Type;
+                item.Comment = attendance.Comment;
+            }
+            foreach (var item in model)
+            {
+                if (item.AttendanceType == null) item.AttendanceType = noSelection;
             }
 
             return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "User")]
+        public ActionResult Edit([Bind] IList<AttendanceSelection> model)
+        {
+            var attendanceTypes = Context.AttendanceTypes.ToList();
+
+            var season = Cache.GetSeason(() => Context.Seasons.GetActualSeason());
+            var teamIDs = Context.Teams.Where(t => t.Season.ID == season.ID).Select(t => t.ID).ToList();
+
+            var eventIDs = Context.Events
+                .Where(e => teamIDs.Contains(e.Team.ID))
+                .Select(m => m.ID)
+                .ToList();
+            var attendances = Context.Attendances.Include("User").Where(a => a.User.ID == Session.UserID && eventIDs.Contains(a.Event.ID)).ToList();
+            var toRemove = new List<Attendance>();
+
+            foreach (var item in model.Where(i => eventIDs.Contains(i.ID)))
+            {
+                var type = attendanceTypes.SingleOrDefault(t => t.ID == item.AttendanceType.ID);
+                if (attendances.Any(a => a.Event.ID == item.ID))
+                {
+                    if (type == null)
+                    {
+                        toRemove.Add(attendances.Single(a => a.Event.ID == item.ID));
+                    }
+                    else
+                    {
+                        var attendance = attendances.Single(a => a.Event.ID == item.ID);
+                        attendance.Type = type;
+                        attendance.Comment = item.Comment;
+                    }
+                }
+                else
+                {
+                    if (type == null) continue;
+
+                    Context.Attendances.Add(new Attendance
+                    {
+                        User = Context.Users.Single(u => u.ID == Session.UserID),
+                        Event = Context.Events.Single(e => e.ID == item.ID),
+                        Type = type,
+                        Comment = item.Comment,
+                    });
+                }
+            }
+            Context.Attendances.RemoveRange(toRemove);
+            Context.SaveChanges();
+
+            return RedirectToAction("Edit");
         }
     }
 }
