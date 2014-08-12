8ef0a0a9280e6d216a53f89963691b7833a54c5c
Raphael Fleischlin <git@raeffs.dev>
2014-08-12 20:59:55 +0200

    log output

--- VolleyRain/VolleyRain/Filter/SessionDataFilterAttribute.cs
+++ VolleyRain/VolleyRain/Filter/SessionDataFilterAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,8 +11,12 @@ namespace VolleyRain.Filter
 {
     public class SessionDataFilterAttribute : ActionFilterAttribute
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
+            Logger.Debug("Going to check whether session data is initialized...");
+
             var session = new SessionDecorator(filterContext.HttpContext.Session);
 
             if (!filterContext.HttpContext.Request.IsAuthenticated)
@@ -21,6 +26,8 @@ namespace VolleyRain.Filter
             }
 
             if (session.IsInitialized) return;
+
+            Logger.Info("Session data is not yet initialized, going to load it...");
 
             using (var context = new DatabaseContext())
             {
