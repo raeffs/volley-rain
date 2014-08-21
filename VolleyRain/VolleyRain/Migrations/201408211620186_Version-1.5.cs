b333002e9bb2caca68efa11568bf90e091d819e9
Raphael Fleischlin <git@raeffs.dev>
2014-08-21 19:41:54 +0200

    version

--- VolleyRain/VolleyRain/Migrations/201408211620186_Version-1.5.cs
+++ VolleyRain/VolleyRain/Migrations/201408211620186_Version-1.5.cs
@@ -11,27 +11,6 @@ namespace VolleyRain.Migrations
             RenameColumn("dbo.TeamMembership", "User_ID", "UserID");
             AddColumn("dbo.TeamMembership", "IsCoach", c => c.Boolean(nullable: false, defaultValue: false));
             AddColumn("dbo.TeamMembership", "IsAdmin", c => c.Boolean(nullable: false, defaultValue: false));
-
-            //DropForeignKey("dbo.TeamMembership", "Team_ID", "dbo.Team");
-            //DropForeignKey("dbo.TeamMembership", "User_ID", "dbo.User");
-            //DropIndex("dbo.TeamMembership", new[] { "Team_ID" });
-            //DropIndex("dbo.TeamMembership", new[] { "User_ID" });
-            //CreateTable(
-            //    "dbo.TeamMembership",
-            //    c => new
-            //        {
-            //            TeamID = c.Int(nullable: false),
-            //            UserID = c.Int(nullable: false),
-            //            IsCoach = c.Boolean(nullable: false),
-            //            IsAdmin = c.Boolean(nullable: false),
-            //        })
-            //    .PrimaryKey(t => new { t.TeamID, t.UserID })
-            //    .ForeignKey("dbo.Team", t => t.TeamID, cascadeDelete: true)
-            //    .ForeignKey("dbo.User", t => t.UserID, cascadeDelete: true)
-            //    .Index(t => t.TeamID)
-            //    .Index(t => t.UserID);
-
-            //DropTable("dbo.TeamMembership");
         }
 
         public override void Down()
@@ -40,25 +19,6 @@ namespace VolleyRain.Migrations
             DropColumn("dbo.TeamMembership", "IsCoach");
             RenameColumn("dbo.TeamMembership", "UserID", "User_ID");
             RenameColumn("dbo.TeamMembership", "TeamID", "Team_ID");
-
-            //CreateTable(
-            //    "dbo.TeamMembership",
-            //    c => new
-            //        {
-            //            Team_ID = c.Int(nullable: false),
-            //            User_ID = c.Int(nullable: false),
-            //        })
-            //    .PrimaryKey(t => new { t.Team_ID, t.User_ID });
-
-            //DropForeignKey("dbo.TeamMembership", "UserID", "dbo.User");
-            //DropForeignKey("dbo.TeamMembership", "TeamID", "dbo.Team");
-            //DropIndex("dbo.TeamMembership", new[] { "UserID" });
-            //DropIndex("dbo.TeamMembership", new[] { "TeamID" });
-            //DropTable("dbo.TeamMembership");
-            //CreateIndex("dbo.TeamMembership", "User_ID");
-            //CreateIndex("dbo.TeamMembership", "Team_ID");
-            //AddForeignKey("dbo.TeamMembership", "User_ID", "dbo.User", "ID", cascadeDelete: true);
-            //AddForeignKey("dbo.TeamMembership", "Team_ID", "dbo.Team", "ID", cascadeDelete: true);
         }
     }
 }
