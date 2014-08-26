471716a1648595d1f092f18983fca829e85c5176
Raphael Fleischlin <git@raeffs.dev>
2014-08-26 19:59:21 +0200

    bbcode editor

---
+++ VolleyRain/VolleyRain/Helper/BBCodeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VolleyRain.Helper
+{
+    public static class BBCodeHelper
+    {
+        public static MvcHtmlString ParseBBCode(this HtmlHelper helper, string value)
+        {
+            var parser = new CodeKicker.BBCode.BBCodeParser(new[] 
+            {
+                new CodeKicker.BBCode.BBTag("b", "<b>", "</b>")
+            });
+            return new MvcHtmlString(parser.ToHtml(value));
+        }
+    }
+}
